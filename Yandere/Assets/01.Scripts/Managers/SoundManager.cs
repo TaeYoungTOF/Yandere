fb25e4b28734200dd34820b591f4c2771035181d
TenChoU <fgd5463@gmail.com>
2025-06-20 20:21:57 +0900

    Feat : SoundManager 수정
    문법 수정 중

--- Yandere/Assets/01.Scripts/Managers/SoundManager.cs
+++ Yandere/Assets/01.Scripts/Managers/SoundManager.cs
@@ -8,10 +8,17 @@ public class SoundManager : MonoBehaviour
     public static SoundManager Instance { get; private set; }
     
     // 사운드의 타입
-    public enum SoundType
+    public enum EBgm
     {
-        BGM,
-        SFX,
+        BGM_TITLE,
+        BGM_GAME,
+    }
+    
+    public enum ESfx
+    {
+        SFX_BUTTON,
+        SFX_ENDING,
+        SFX_CLCIK,
     }
 
     private void Awake()
