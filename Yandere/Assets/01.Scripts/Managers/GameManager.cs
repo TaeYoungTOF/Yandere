5834d0b4fc8947daacb1287adbd75cd0ac3895a7
TaeYoungTOF <113530625+TaeYoungTOF@users.noreply.github.com>
2025-06-20 16:41:19 +0900

    [Init] Initial Commit
    - Initial Commit

---
+++ Yandere/Assets/01.Scripts/Managers/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    public MoneyManager MoneyManager { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    
+}
---
+++ Yandere/Assets/01.Scripts/Managers/SoundManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+    
+}
