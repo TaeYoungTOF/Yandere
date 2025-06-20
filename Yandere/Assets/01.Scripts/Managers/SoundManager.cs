535db5143e8e77beaf769010693332cdd1661feb
TenChoU <fgd5463@gmail.com>
2025-06-20 19:26:50 +0900

    Feat : SoundManager 수정
    간단한 선언

--- Yandere/Assets/01.Scripts/Managers/SoundManager.cs
+++ Yandere/Assets/01.Scripts/Managers/SoundManager.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
+    
+    // 사운드의 타입
+    public enum SoundType
+    {
+        BGM,
+        SFX,
+    }
 
     private void Awake()
     {
