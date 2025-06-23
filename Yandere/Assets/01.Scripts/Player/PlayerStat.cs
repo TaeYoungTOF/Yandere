e4389afd1c36883e6ba7d7d3e5beb68eaf06698b
kimhhjj <gksdnf1984@naver.com>
2025-06-23 12:34:11 +0900

    [feat] PlayerStat 추가
    [feat] PlayerStat 능력치 추가

---
+++ Yandere/Assets/01.Scripts/Player/PlayerStat.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStat
+{
+    public int BaseAttack { get; set; }
+    public int BaseArmor { get; set; }
+    public int CurrentHealth { get; set; }
+    public int MaxHealth { get; set; }
+    public float HealthRegen { get; set; }
+    public int CurrentEXP { get; set; }
+    public int MaxEXP { get; set; }
+    public float PlayerMoveSpeed { get; set; }
+
+}
