2d63e18b1c84d61e13301ee32c535ed3d950bb42
TenChoU <fgd5463@gmail.com>
2025-06-23 11:34:21 +0900

    FIX : Damage 부분 수정
    스크립트 자체 수정
    
    언어 구조 변경

--- Yandere/Assets/01.Scripts/Enemies/EnemyData.cs
+++ Yandere/Assets/01.Scripts/Enemies/EnemyData.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "EnemyCneter", menuName = "ScrptableObject/EnemyData", order = 1)]
 public class EnemyData : ScriptableObject
 {
+    
     public GameObject Prefab;
     public string Name;
     public float HP;
