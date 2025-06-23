597abab16f38119457b4cd2be96cfb0ca75ceec6
TenChoU <fgd5463@gmail.com>
2025-06-23 11:25:52 +0900

    Feat : Enemy 업데이트
    사양 변경
    
    오디오매니저 + 적 스크립트 추가 사양
    
    // 오디오매니저 추후 수정

---
+++ Yandere/Assets/01.Scripts/Enemies/EnemyData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyData : ScriptableObject
+{
+    public GameObject Prefab;
+    public string Name;
+    public float HP;
+    public float Damage;
+}
---
+++ Yandere/Assets/01.Scripts/Managers/DamageManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DamageManager : MonoBehaviour
+{
+    public class IDamagble
+   {
+       public void TakeDamage(float damage)
+       {
+           return;
+       }
+   }
+}
