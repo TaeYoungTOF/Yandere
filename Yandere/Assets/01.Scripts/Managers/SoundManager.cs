ff2b5f2f1bfe578f1ceaa1a8662764a58674a556
TenChoU <fgd5463@gmail.com>
2025-06-23 14:51:18 +0900

    FIX : SoundManager 수정

--- Yandere/Assets/01.Scripts/Managers/SoundManager.cs
+++ Yandere/Assets/01.Scripts/Managers/SoundManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.Audio;
 
 public class SoundManager : MonoBehaviour
 {
-    public static SoundManager Instance { get; private set; }
+    public static SoundManager Instance;
     
     // 사운드의 타입
     public enum EBgm
@@ -23,7 +23,7 @@ public class SoundManager : MonoBehaviour
     
     // audioclop 담을 수 있는 배열
     [SerializeField] private AudioClip[] bgms;
-    [SerializeField] private AudioMixerGroup[] sfxs;
+    [SerializeField] private AudioClip[] sfxs;
     
     // 플레이 하는 audiosource
     [SerializeField] AudioSource audioBgm;
@@ -39,4 +39,23 @@ public class SoundManager : MonoBehaviour
         Instance = this;
     }
     
+    // EBgm 열거형을 매개변수로 받아 해당되는 배경음악 클립을 재생
+    public void PlayBGM(EBgm bgmidx)
+    {
+        //enum int형으로 형변환
+        audioBgm.clip = bgms[(int)bgmidx];
+        audioBgm.Play();
+    }
+    
+    // 현재 재생 중인 배경 음악 정지
+    public void StopBGM()
+    {
+        audioBgm.Stop();
+    }
+    
+    // ESfx를 열거형을 매개변수로 받아 해당되는 효과음을 재생
+    public void PlaySFX(ESfx esfx)
+    {
+        audioSfx.PlayOneShot(sfxs[(int)esfx]);
+    }
 }
