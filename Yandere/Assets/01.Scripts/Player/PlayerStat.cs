ace3d9866e5435eb7d6e56a6a9c58e21e8e27292
kimhhjj <gksdnf1984@naver.com>
2025-06-23 22:03:59 +0900

    [feat] Joystick, IDamagable 추가 [fix] PlayerStat 에 CurrentGold 추가
    [feat] PlayerController
    Joystick(조이스틱) 기능 추가
    IDamagable(인터페이스)에 TakeDamage 추가
    
    [fix] PlayerStat
    PlayerStat에 CurrentGold 필드 추가

--- Yandere/Assets/01.Scripts/Player/PlayerStat.cs
+++ Yandere/Assets/01.Scripts/Player/PlayerStat.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlayerStat
 {
-    public int BaseAttack { get; set; }
-    public int BaseArmor { get; set; }
-    public int CurrentHealth { get; set; }
-    public int MaxHealth { get; set; }
-    public float HealthRegen { get; set; }
-    public int CurrentEXP { get; set; }
-    public int MaxEXP { get; set; }
-    public float PlayerMoveSpeed { get; set; }
+    public int BaseAttack { get; set; }                 //플레이어 기본 공격력
+    public int BaseArmor { get; set; }                  //플레이어 기본 아머
+    public int CurrentHealth { get; set; }              //플레이어 현재 체력
+    public int MaxHealth { get; set; }                  //플레이어 최대 체력
+    public float HealthRegen { get; set; }              //플레이어 체력 재생
+    public int CurrentEXP { get; set; }                 //플레이어 현재 경험치
+    public int MaxEXP { get; set; }                     //플레이어 최대 경험치
+    public float PlayerMoveSpeed { get; set; }          //플레이어 이동속도
+    public int CurrentGold { get; set; }                //플레이어 현재 보유골드
 
 }
