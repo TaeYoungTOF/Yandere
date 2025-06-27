32a6d6f5efa11274cf733f88e1068f26fb6bf003
TaeYoungTOF <113530625+TaeYoungTOF@users.noreply.github.com>
2025-06-27 20:29:15 +0900

    [Combine] 06/27 코드 병합
    - 코드 병합

--- Yandere/Assets/01.Scripts/Managers/LevelUpManager.cs
+++ Yandere/Assets/01.Scripts/Managers/LevelUpManager.cs
@@ -1,36 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class LevelUpManager : MonoBehaviour
-{
-    public SkillSelectUI skillSelectUI;
-    public List<BaseSkill> allSkills;
+// public class LevelUpManager : MonoBehaviour
+// {
+//     public SkillSelectUI skillSelectUI;
+//     public List<BaseSkill> allSkills;
 
-    public void OnLevelUp()
-    {
-        Debug.Log("레벨업 매니저 - 스킬 선택 UI 호출됨");
-        var options = GetRandomSkillOptions(3);
-        skillSelectUI.Show(options);
-    }
+//     public void OnLevelUp()
+//     {
+//         Debug.Log("레벨업 매니저 - 스킬 선택 UI 호출됨");
+//         var options = GetRandomSkillOptions(3);
+//         skillSelectUI.Show(options);
+//     }
 
-    private List<BaseSkill> GetRandomSkillOptions(int count)
-    {
-        List<BaseSkill> available = new List<BaseSkill>();
-        foreach (var skill in allSkills)
-        {
-            if (!FindObjectOfType<SkillManager>().equippedSkills.Contains(skill) || skill.level < 5)
-                available.Add(skill);
-        }
+//     private List<BaseSkill> GetRandomSkillOptions(int count)
+//     {
+//         List<BaseSkill> available = new List<BaseSkill>();
+//         foreach (var skill in allSkills)
+//         {
+//             if (!FindObjectOfType<SkillManager>().equippedSkills.Contains(skill) || skill.level < 5)
+//                 available.Add(skill);
+//         }
 
-        List<BaseSkill> result = new List<BaseSkill>();
-        for (int i = 0; i < count; i++)
-        {
-            if (available.Count == 0) break;
-            int rand = Random.Range(0, available.Count);
-            result.Add(available[rand]);
-            available.RemoveAt(rand);
-        }
-        return result;
-    }
-}
+//         List<BaseSkill> result = new List<BaseSkill>();
+//         for (int i = 0; i < count; i++)
+//         {
+//             if (available.Count == 0) break;
+//             int rand = Random.Range(0, available.Count);
+//             result.Add(available[rand]);
+//             available.RemoveAt(rand);
+//         }
+//         return result;
+//     }
+// }
