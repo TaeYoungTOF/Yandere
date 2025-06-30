6e48c1f6267c4924f038aff97801845b4ae88e10
TaeYoungTOF <113530625+TaeYoungTOF@users.noreply.github.com>
2025-06-30 09:52:38 +0900

    [chore] Item 관련 작업
    - Item 관련 작업(진행 중)

--- Yandere/Assets/01.Scripts/Data/DropTable.cs
+++ Yandere/Assets/01.Scripts/Data/DropTable.cs
@@ -1,22 +1,15 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
 
-[System.Serializable]
-public class ItemData
-{
-    public string itemId;
-    public GameObject prefab;
-}
-
-[CreateAssetMenu(fileName = "DropTable", menuName = "Drop/DropTable", order = 1)]
+[CreateAssetMenu(fileName = "DropTable", menuName = "DropTable", order = 1)]
 public class DropTable : ScriptableObject
 {
     [System.Serializable]
     public class DropEntry
     {
-        public ItemData itemData;
-        [Range(0f, 1f)]
-        public float probability;
+        public GameObject itemPrefab;
+
+        [Range(0f, 1f)] public float probability;
     }
 
     public List<DropEntry> entries = new();
