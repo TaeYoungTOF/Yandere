c8d5fea4fcb9b21a08816f2c451548e7528f671b
TaeYoungTOF <113530625+TaeYoungTOF@users.noreply.github.com>
2025-07-09 10:02:42 +0900

    [fir] 계산식 수정
    - 경험치 계산식 수정
    - 스킬폴더명 수정

---
+++ Yandere/Assets/01.Scripts/Skill/Skill/2_BurstingGaze/BurstingGaze.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BurstingGaze : ActiveSkill
+{
+    public override void TryActivate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateActiveData()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void UpdateCooldown()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Activate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Yandere/Assets/01.Scripts/Skill/Skill/2_BurstingGaze/BurstingGazeProjectile.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class BurstingGazeProjectile : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
