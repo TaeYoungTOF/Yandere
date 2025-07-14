602a9219159a3ed227c588d9ebef5bd5cf67a9d2
kimhhjj <gksdnf1984@naver.com>
2025-07-14 10:06:22 +0900

    [feat] ItemSpawnerManager, Item_MagnetEffect, Item_ItemDropEffect 추가 [fix] Player 수정
    [feat] ItemSpawnerManager, Item_MagnetEffect, Item_ItemDropEffect
    
    ItemSpawnerManager : 필드오브젝트의 스폰을 관리함
    
    Item_MagnetEffect : 필드에  드랍된 아이템들을 관리함
    
    Item_ItemDropEffect : 자석효과이펙트를 관리함
    
    [fix] Player
    
    플레이어한테 Item_MagnetEffect를 달아 줌

--- Yandere/Assets/01.Scripts/Item/ItemObject.cs
+++ Yandere/Assets/01.Scripts/Item/ItemObject.cs
@@ -1,23 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemObject : MonoBehaviour, IDamagable
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float objectMaxHealth = 10;
+    [SerializeField] private DropContext _dropContext;
+    private float currentHealth;
+    
+    private bool isBroken = false;
+    private Animator _animator;
+    
+
+    void Awake()
     {
-        
+        currentHealth = objectMaxHealth;
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("게임 오브젝트 테스트 단축키 : E");
+            TakeDamage(10f);
+        }
     }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("PlayerSkill"))
+        {
+            TakeDamage(10f);
+        }
+    }
+    
     public void TakeDamage(float damage)
     {
+        if(isBroken) return;
+        
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            ObjectBreak();
+        }
+    }
+    
+    void ObjectBreak()
+    {
+        isBroken = true;
+        
+        Debug.Log("오브젝트가 파괴 됨");
+        _animator.SetTrigger("Break");
+        _dropContext.position = transform.position;
+        StageManager.Instance.ItemDropManager.HandleDrop(_dropContext);
         
     }
+    
+    public void ObjectBreakAnimationEnd()
+    {
+        Destroy(gameObject);
+    }
+
 }
--- Yandere/Assets/01.Scripts/Item/Item_Magnet.cs
+++ Yandere/Assets/01.Scripts/Item/Item_Magnet.cs
@@ -6,7 +6,18 @@ public class Item_Magnet : Item
     {
         Debug.Log("[Magnet] Magnet");
 
-        Destroy(gameObject);
+        // 플레이어에서 MagnetEffect를 가져와 발동
+        Item_MagnetEffect magnet = player.GetComponent<Item_MagnetEffect>();
+        if (magnet != null)
+        {
+            magnet.AttractAllItems();
+        }
+        else
+        {
+            Debug.Log("MagnetEffect 컴포넌트를 Player에서 찾을 수 없습니다!");
+        }
+
+        Destroy(gameObject); // 아이템 자체는 파괴
     }
 
     private void OnTriggerEnter2D(Collider2D other)
